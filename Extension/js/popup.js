105da92b581b587f039ed377e9dda0788ba89d27
Tanmay Kenjale <thkenjale@gmail.com>
2020-07-06 00:34:49 -0400

    made single feedback function to handle yes/no click events

--- Extension/js/popup.js
+++ Extension/js/popup.js
@@ -5,51 +5,29 @@ $(document).ready(function() {
             activeTabURL = activeTab.url;
 
             $.ajax({
-                url: 'http://127.0.0.1:5000/',
+                url: 'http://127.0.0.1:5000/predict',
                 type: "POST",
                 data: { 'url': activeTabURL },
                 dataType: "json",
                 success: function(data) {
                     $("#score").text('Score: ' + data['combined prob'] + '% reliable');
                     $("#question").show()
-                    $("#yes").show();
-                    $('#no').show();
+                    $(".feedback").show();
                 }
             });
         });
     });
 
-    $("#yes").click(function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            var activeTab = tabs[0];
-            activeTabURL = activeTab.url;
-
-            $.ajax({
-                url: 'http://127.0.0.1:5000/',
-                type: "POST",
-                data: { 'url': activeTabURL },
-                dataType: "json",
-                success: function(data) {
-
-                }
-            });
-        });
-    });
-
-    $("#no").click(function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            var activeTab = tabs[0];
-            activeTabURL = activeTab.url;
-
-            $.ajax({
-                url: 'http://127.0.0.1:5000/',
-                type: "POST",
-                data: { 'url': activeTabURL },
-                dataType: "json",
-                success: function(data) {
-
-                }
-            });
+    $(".feedback").click(function() {
+        $.ajax({
+            url: 'http://127.0.0.1:5000/feedback',
+            type: "POST",
+            data: { 'feedback': $(this).text() },
+            dataType: "json",
+            success: function(data) {
+                $("#question").hide()
+                $(".feedback").hide();
+            }
         });
     });
 });
